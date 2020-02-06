dcaecdc6d238799bd1c0d5b38dfb0b110f4e1750
Andy Ramirez <36055682+AndyFco@users.noreply.github.com>
2020-02-05 22:29:35 -0400

    Insertando Pacientes

--- registrarPaciente.php
+++ registrarPaciente.php
@@ -2,36 +2,28 @@
 <html>
 <head>
 <title>Nuevo Paciente</title>
-
 <link rel="stylesheet" type="text/css" href="css/normalize.css" />
 <link rel="stylesheet" type="text/css" href="css/styles.css" />
-
 </head>
 <body>
-
 	<header>
 		<h1>Sistema de Facturacion de Servicios de Salud </h1>
   </header>
-
   <a href="main.php">
-   <img src="images/logo.png" alt="Logo">
-  </a>
-    
+   <img src="images/logo.png" alt="Logo"></a>
+
   <div class="formulario" >
-     <form action="main.php" method="POST">
+     <form action="utilidades/registrarPaciente.php" method="POST">
         <h1>Nuevo Pacente</h1>
-        <input type="text" placeholder="Nombre " name="nombre" required>
-        <input type="text" placeholder="Apellido " name="apellido" required><br>
-        <input type="textShort" placeholder="Cedula " name="cedula" required>
-        <input type="textShort" placeholder="Telefono " name="telefono" required>
-        <input type="text" placeholder="Direccion " name="direccion" required>
+        <input type="text"      placeholder="Nombre"    name="nombre"    id="nombre" required>
+        <input type="text"      placeholder="Apellido"  name="apellido"  id="apellido" required><br>
+        <input type="textShort" placeholder="Cedula"    name="cedula"    id="cedula" required>
+        <input type="textShort" placeholder="Telefono"  name="telefono"  id="telefono" required>
+        <input type="text"      placeholder="Direccion" name="direccion" id="direccion" required>
         <br>
-        <button type="submit" class="savebtn" formaction="main.php"><b> Guardar</b></button>
+        <button type="submit" class="savebtn" ><b> Guardar</b></button>
         <button type="reset" class="savebtn cancelbtn" ><b>Cancelar</b></button>
      </form>
    </div>
-        
-    
-
 </body>
 </html>
---
+++ utilidades/registrarPaciente.php
@@ -0,0 +1,28 @@
+<?php
+include 'conection.php';
+$nombre =$_POST["nombre"];
+$apellido =$_POST["apellido"];
+$cedula =$_POST["cedula"];
+$telefono =$_POST["telefono"];
+$direccion =$_POST["direccion"];
+
+$insertar= 'INSERT INTO pacientes'
+."(nombre, apellido, cedula, telefono, direccion) 
+VALUES ("."'".$nombre."'," 
+         ."'".$apellido."'," 
+         ."'".$cedula. "'," 
+         ."'".$telefono. "'," 
+         ."'".$direccion."'  "
+         .	 ");";
+
+try{
+    require_once("conection.php");
+    $resultado =$con->query($insertar);
+    }catch(\Exception $e){echo $e->getMessage();}
+mysqli_close($con);
+?>
+<script> 
+
+window.location.replace("../main.php"); 
+
+</script>
